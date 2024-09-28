998ea44258a35fac2906806e13d4b510a7a614bd
Jorge <jorgomez@protonmail.com>
2024-09-28 23:00:35 +0100

    up

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import { Footer } from './src/components/Footer/Footer'
 import { Header } from './src/components/Header/Header'
 import { Main } from './src/components/Main/Main'
-import { Events } from './src/pages/Events/Events'
+
+import { Home } from './src/pages/Home/Home'
 
 import './style.css'
 
 Header()
 Main()
-Events()
+Home()
 Footer()
